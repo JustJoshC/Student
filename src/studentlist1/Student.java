830da4100a5786eb99f77633fcbefa318d1bd12d
Josh Cowan <cowanjos@sheridancollege.ca>
2020-05-28 10:39:55 -0400

    PartTime branch added new property

--- src/studentlist1/Student.java
+++ src/studentlist1/Student.java
@@ -2,18 +2,41 @@ package studentlist1;
 
 /**
  *
+ *
  * @author Josh Cowan -- May 2020
  */
-public class Student{
+public class Student
+{
 
-private String name;
+    private String name;
+    private String numOfHours;
+
+    /**
+     * Get the value of numOfHours
+     *
+     * @return the value of numOfHours
+     */
+    public String getNumOfHours()
+    {
+        return numOfHours;
+    }
+
+    /**
+     * Set the value of numOfHours
+     *
+     * @param numOfHours new value of numOfHours
+     */
+    public void setNumOfHours(String numOfHours)
+    {
+        this.numOfHours = numOfHours;
+    }
 
     /**
      * Get the value of name
      *
      * @return the value of name
      */
-    public String getName() 
+    public String getName()
     {
         return name;
     }
@@ -23,7 +46,8 @@ private String name;
      *
      * @param name new value of name
      */
-    public void setName(String name) {
+    public void setName(String name)
+    {
         this.name = name;
     }
 }
